ad1f1a1d3609159819486c61811e29b25a1bbb45
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-12-06 07:21:12 +0300

    added provider for the app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import store from "./redux/store"
 
 
 function App() {
- let data =[
-              {
-                id:1,
-                name:"to-do",
-                tasks:[{ id:1,name:"to use redux"},{ id:2,name:"to complete task tomorrow"}]
-              },{
-                id:2,
-                name:"In Progress",
-                tasks:[{ id:1,name:"to complete absenteeism report"},{ id:2,name:"to complete task tomorrow"}]
-              }
-          ]
 
   return (
-    
       <Provider store={store}>
         <ColumnContainer/>
       </Provider>
