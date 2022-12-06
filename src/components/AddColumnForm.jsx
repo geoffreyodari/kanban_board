c0737fb331ba7df888fc7e44c88c2e3893cdb863
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-12-06 07:08:32 +0300

    added form for adding columns

--- src/components/AddColumnForm.jsx
+++ src/components/AddColumnForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import {connect} from 'react-redux'
 import { addColumn } from '../redux';
 
 function AddColumnForm(props) {
-    
+    const [title,setTitle] = useState("")
 
     return (    <React.Fragment>
-                        <div className="bg-white pt-3">
-                                <Form.Control size="sm" type="text" placeholder="Add Column" />
+                        <div className="bg-white pt-3" key="odari">
+                                <Form.Control size="sm" type="text" placeholder="Add Column" onChange={e=>setTitle(e.target.value)}/>
                                 <div className="row pt-1 pb-1">
                                         <div className="col-6 text-left">
                                                 <Button  variant="danger" size="sm" onClick={()=>props.setShow(false)}>
@@ -17,7 +18,7 @@ function AddColumnForm(props) {
                                                 </Button>
                                         </div>
                                         <div className="col-6 text-end">
-                                                <Button variant="success" size="sm" onClick={props.addColumn }>
+                                                <Button variant="success" size="sm" onClick={()=>props.addColumn(title)}>
                                                         Add Column
                                                 </Button>
                                         </div>
@@ -36,7 +37,7 @@ function AddColumnForm(props) {
     
     const mapDispatchToProps = dispatch=>{
         return{
-            addColumn:()=> dispatch(addColumn())
+            addColumn:(columnTitle)=> dispatch(addColumn(columnTitle))
         }
     }
     export default connect(mapStateToProps,mapDispatchToProps)(AddColumnForm)
