9be5493cba7d27a82700d5fc9de7742919613874
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-12-06 07:15:01 +0300

    added actions for new column and new task card

--- src/redux/Column/columnActions.js
+++ src/redux/Column/columnActions.js
@@ -1,16 +1,18 @@
 import { ADD_COLUMN } from "./columnTypes"
 import { ADD_CARD } from "./columnTypes"
 
-export const addColumn =()=>{
+export const addColumn =(columnTitle)=>{
                 return{
-                    type: ADD_COLUMN
+                    type: ADD_COLUMN,
+                    payload:columnTitle
                 }
         
             }
 
-export const addCard =()=>{
+export const addCard =(column=0,taskName="")=>{
                 return{
-                    type: ADD_CARD
+                    type: ADD_CARD,
+                    payload:{column,taskName}
                 }
 
             }
