b72874b555aeb834757a5b14ab780205a035e1c6
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-12-06 07:05:10 +0300

    added form for adding cards

--- src/components/AddCardForm.jsx
+++ src/components/AddCardForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import {connect} from 'react-redux'
 import { addCard } from '../redux';
 
 function AddCardForm(props) {
-    
+    const [taskName,setTaskName] = useState("")
 
     return (    <React.Fragment>
                         <div className="bg-white pt-3">
-                                <Form.Control size="sm" type="text" placeholder="Add Card" />
+                                <Form.Control size="sm" type="text" placeholder="Add Card" onChange={e=>setTaskName(e.target.value)}/>
                                 <div className="row pt-1 ">
                                         <div className="col-6 text-start">
                                                 <Button  variant="danger" size="sm" onClick={()=>props.setShow(false)}>
@@ -17,8 +18,8 @@ function AddCardForm(props) {
                                                 </Button>
                                         </div>
                                         <div className="col-6 text-end">
-                                                <Button variant="success" size="sm" onClick={props.addCard}>
-                                                        Add 
+                                                <Button variant="success" size="sm" onClick={()=>{props.addCard(props.columnid,taskName)}} >
+                                                        Add
                                                 </Button>
                                         </div>
                                 </div>
@@ -38,7 +39,7 @@ function AddCardForm(props) {
     
     const mapDispatchToProps = dispatch=>{
         return{
-            addCard:()=> dispatch(addCard())
+            addCard:(column,taskName)=> dispatch(addCard(column,taskName))
         }
     }
 
