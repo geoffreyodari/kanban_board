2dd09cab09144f0a91690185fbd3ec9a587bd18a
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-12-06 07:07:17 +0300

    added container for columns component

--- src/components/ColumnContainer.jsx
+++ src/components/ColumnContainer.jsx
@@ -5,11 +5,12 @@ import {connect} from 'react-redux'
 
 
 function ColumnContainer(props) {
-    console.log(props.state)
+    
   return (
+    
     <div className="container-fluid row bg-light vh-100">
-        {props.state.map((col,key)=><Column data={col} key={key}/>)}
-        <AddColumn/>
+        {props.state.map((item,index)=><Column data={item} key={index} columnid={index}/>)}
+        <AddColumn key={"z"}/>
     </div>
   )
 }
